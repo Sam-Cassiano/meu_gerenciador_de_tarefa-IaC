28fb21007a4edb0363e575e54ffd8fb9c664051f
Sam-Cassiano <103199805+Sam-Cassiano@users.noreply.github.com>
2025-12-03 09:45:06 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -8,11 +8,10 @@ terraform {
       s3 = "https://sfo3.digitaloceanspaces.com"
     }
 
+    # Ignorar validações de credenciais e região
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
 
     use_path_style = true
   }
