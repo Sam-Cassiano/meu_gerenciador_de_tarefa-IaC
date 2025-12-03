d5664d2924bccaf606f03a8fb959be05a009d7a8
Sam-Cassiano <103199805+Sam-Cassiano@users.noreply.github.com>
2025-12-03 08:57:34 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -7,10 +7,13 @@ terraform {
     endpoints = {
       s3 = "https://sfo3.digitaloceanspaces.com"
     }
- 
+
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    use_path_style              = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+
+    use_path_style = true
   }
 }
