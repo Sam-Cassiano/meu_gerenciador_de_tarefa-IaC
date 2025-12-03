4a18de13cc047ee5f73b85151c15e3e45c6b507d
Sam-Cassiano <103199805+Sam-Cassiano@users.noreply.github.com>
2025-12-02 22:59:09 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,18 +1,19 @@
 terraform {
   backend "s3" {
-    bucket = "meu-gerenciador-de-tarefas-sam"   # Nome do seu Space
-    key    = "terraform.tfstate"                # Caminho do arquivo de estado
-    region = "us-east-1"                        # Região (pode ser qualquer, DigitalOcean ignora)
+    bucket = "meu-gerenciador-de-tarefas-sam"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
 
     endpoints = {
-      s3 = "https://sfo3.digitaloceanspaces.com"  # Endpoint do Space
+      s3 = "https://sfo3.digitaloceanspaces.com"
     }
 
-    # Ignora algumas validações para funcionar melhor com DigitalOcean Spaces
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
+    skip_requesting_account_id  = true
+    skip_s3_checksum            = true
 
-    use_path_style = true   # Necessário para DigitalOcean Spaces
+    use_path_style = true
   }
 }
