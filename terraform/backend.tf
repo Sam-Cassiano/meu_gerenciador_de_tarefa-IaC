98fd8961c31bb62b31fd223d342756562627ddfe
Sam-Cassiano <103199805+Sam-Cassiano@users.noreply.github.com>
2025-12-03 08:59:01 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -11,9 +11,6 @@ terraform {
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-
-    use_path_style = true
+    use_path_style              = true
   }
 }
