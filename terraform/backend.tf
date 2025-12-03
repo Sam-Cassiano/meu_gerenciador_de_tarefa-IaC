17c0af02e3a19714ba2dcfa6f6ac9114e6329154
Sam-Cassiano <103199805+Sam-Cassiano@users.noreply.github.com>
2025-12-03 09:29:50 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,16 +1,16 @@
 terraform {
   backend "s3" {
-    bucket = "meu-gerenciador-de-tarefas-sam"
-    key    = "terraform.tfstate"
+    bucket = "meu-gerenciador-de-tarefas-sam"   # Nome do seu Space
+    key    = "terraform.tfstate"                # Caminho do estado
     region = "us-east-1"
 
     endpoints = {
-      s3 = "https://sfo3.digitaloceanspaces.com"
+      s3 = "https://sfo3.digitaloceanspaces.com"  # Endpoint do Space
     }
 
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
-    use_path_style              = true 
+    use_path_style              = true
   }
 }
