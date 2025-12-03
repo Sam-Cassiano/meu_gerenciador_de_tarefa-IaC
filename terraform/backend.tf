b0699e9d3aa005333555ae1089cf550b2d1f6c65
Sam-Cassiano <103199805+Sam-Cassiano@users.noreply.github.com>
2025-12-03 14:02:29 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,7 +5,7 @@ terraform {
     region = "us-east-1"
 
     endpoints = {
-      s3 = "https://nyc3.digitaloceanspaces.com"
+      s3 = "https://sfo3.digitaloceanspaces.com"
     }
 
     skip_credentials_validation = true
