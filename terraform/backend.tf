5cf9748862b4cbac873605e4702905b5db44b150
Sam-Cassiano <103199805+Sam-Cassiano@users.noreply.github.com>
2025-12-03 09:17:11 -0300

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,14 +4,16 @@ terraform {
     key    = "terraform.tfstate"
     region = "us-east-1"
 
+    # Endpoint do DigitalOcean Spaces
     endpoints = {
       s3 = "https://sfo3.digitaloceanspaces.com"
     }
 
+    # Permite usar o Spaces sem validação AWS
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_region_validation      = true
     use_path_style              = true
   }
 }
- 
+
